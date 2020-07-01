51ab498be8bf9b8ccdba57e5630ffab762268983
denliedesign <customdenlie@gmail.com>
2020-07-01 13:39:47 -0500

    testimonials fix

--- resources/views/mobile.blade.php
+++ resources/views/mobile.blade.php
@@ -16,4 +16,43 @@
         </div>
     </div>
 
+    <div id="carouselTestimonials" class="carousel slide mt-2" data-ride="carousel" style="font-family: 'Courgette', cursive; font-size: 1.5em;">
+        <div class="carousel-inner text-center">
+            <div class="carousel-item active">
+                <p style="line-height: 1;">Our happy place
+                    <br><span class="quotee">- Laura D</span>
+                </p>
+            </div>
+            <div class="carousel-item">
+                <p style="line-height: 1;">Boys dance too!
+                    <br><span class="quotee">- Brandon N</span>
+                </p>
+            </div>
+            <div class="carousel-item">
+                <p style="line-height: 1;">The place to be
+                    <br><span class="quotee">- Chris G</span>
+                </p>
+            </div>
+            <div class="carousel-item">
+                <p style="line-height: 1;">Very nurturing
+                    <br><span class="quotee">- Zoie B</span>
+                </p>
+            </div>
+            <div class="carousel-item">
+                <p style="line-height: 1;">Our dance family
+                    <br><span class="quotee">- The Hollands</span>
+                </p>
+            </div>
+            <div class="carousel-item">
+                <p style="line-height: 1;">Quality and fun
+                    <br><span class="quotee">- Kerri A</span>
+                </p>
+            </div>
+            <div class="carousel-item">
+                <p style="line-height: 1;">Our second family
+                    <br><span class="quotee">- Jenn Z</span>
+                </p>
+            </div>
+        </div>
+    </div>
 </div>
