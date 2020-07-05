d78393aeef7da752bacf3d87e785988a1f4e6065
denliedesign <customdenlie@gmail.com>
2020-07-05 14:08:04 -0500

    SEO

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,11 +3,17 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="og:title" content="Dance Classes | Kings Park & Northport, NY">
+    <meta property="og:description" content="For Over 30 Years. One Stop Shop For Dance, Acting, And Voice. Creating Community For All Ages!">
+    <meta property="og:image" content="https://kpdancecenter.com/images/kings-park-dance-bench.jpeg">
+    <meta property="og:url" content="https://kpdancecenter.com">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="description" content="@yield('description', 'For Over 30 Years. One Stop Shop For Dance, Acting, And Voice. Creating Community For All Ages!')">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'Kings Park Dancer Center | New York')</title>
+    <title>@yield('title', 'Kings Park Dancer Center | Kings Park & Northport, NY')</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -21,6 +27,13 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon/site.webmanifest">
+
 </head>
 <body>
 
