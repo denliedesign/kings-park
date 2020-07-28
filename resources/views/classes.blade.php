1fa31e67387131a6d6618452462dcdecb85702f6
denliedesign <customdenlie@gmail.com>
2020-07-28 11:46:20 -0500

    add fall classes

--- resources/views/classes.blade.php
+++ resources/views/classes.blade.php
@@ -58,19 +58,34 @@
                 </div>
             </div>
             <div class="card">
-                <div class="card-header class-cat-4" id="headingThree">
+                <div class="card-header class-cat-4" id="headingFour">
                     <h2 class="mb-0">
                         <button class="btn btn-link btn-block text-center collapsed" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                            High School Musical Workshop
+                            Northport Fall Dance Classes
                         </button>
                     </h2>
                 </div>
                 <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionExample">
                     <div class="card-body">
-                        <script type="text/javascript" src="https://app.jackrabbitclass.com/jr3.0/Openings/OpeningsJS?OrgID=540329&Session=2020%20High%20School%20Musical&sort=StartDate,Days,StartTime"></script>
+                        <script type="text/javascript" src="https://app.jackrabbitclass.com/jr3.0/Openings/OpeningsJS?OrgID=540329&Loc=NPT&session=2020%20-%202021%20Fall%20Season&sort=StartDate,Days,StartTime&Hidecols=Ages,Openings"></script>
                     </div>
                 </div>
             </div>
+            <div class="card">
+                <div class="card-header class-cat-1" id="headingFive">
+                    <h2 class="mb-0">
+                        <button class="btn btn-link btn-block text-center collapsed" type="button" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                            Kings Park Fall Dance Classes
+                        </button>
+                    </h2>
+                </div>
+                <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordionExample">
+                    <div class="card-body">
+                        <script type="text/javascript" src="https://app.jackrabbitclass.com/jr3.0/Openings/OpeningsJS?OrgID=540329&Loc=KP&session=2020%20-%202021%20Fall%20Season&sort=StartDate,Days,StartTime&Hidecols=Ages,Openings"></script>
+                    </div>
+                </div>
+            </div>
+
         </div>
 
     </div>
