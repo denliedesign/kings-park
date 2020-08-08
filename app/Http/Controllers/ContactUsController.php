878cc4850b27fb1256cd5e7bbba419c6c0091f3e
denliedesign <customdenlie@gmail.com>
2020-08-08 10:49:02 -0500

    mail to kpdancecenter gmail

--- app/Http/Controllers/ContactUsController.php
+++ app/Http/Controllers/ContactUsController.php
@@ -30,7 +30,7 @@ class ContactUsController extends Controller
             'message' => 'required',
         ]);
 
-        Mail::to('kpdcdirector@gmail.com')->send(new ContactUsMail($data));
+        Mail::to('kpdancecenter@gmail.com')->send(new ContactUsMail($data));
 
         return redirect('/')->with('message', 'Thanks for your message. We\'ll be in touch.');
 
