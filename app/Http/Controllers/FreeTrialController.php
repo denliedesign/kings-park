78f9ed19a25c4330d33b0730f8f226394a3494d3
denliedesign <customdenlie@gmail.com>
2020-09-18 09:25:07 -0500

    trial email to kpdc

--- app/Http/Controllers/FreeTrialController.php
+++ app/Http/Controllers/FreeTrialController.php
@@ -30,7 +30,7 @@ class FreeTrialController extends Controller
             'class' => 'required'
         ]);
 
-        Mail::to('customdenlie@gmail.com')->send(new FreeTrialMail($trial));
+        Mail::to('kpdcdirector@gmail.com')->send(new FreeTrialMail($trial));
 
         return redirect('/')->with('message', 'Thank you for your interest. We wil contact you shortly.');
     }
